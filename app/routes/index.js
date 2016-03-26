9f2a90790a4083cbb21f7ac77cfb5dec902550a1
Ajoy Oommen <ajoy91ad@gmail.com>
2016-03-26 12:55:28 +0530

    Return radio player in route model

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,38 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+    model(params) {
+        var radio = {
+            isPlaying: false,
+            isBuffering: false,
+            _audio: null,
+            radio_url: null,
+            create: function() {
+                if (!this._audio) {
+                    var url = this.radio_url;
+                    this._audio = new Audio(url);
+                }
+            },
+            start_playing: function() {
+                this.isPlaying = true;
+            },
+            stop_playing: function() {
+                this.isPlaying = false;
+            },
+            play: function() {
+                if (this._audio) {
+                    this._audio.load();
+                    this._audio.play();
+                    this.start_playing();
+                }
+            },
+            stop: function() {
+                if (this._audio) {
+                    this._audio.pause();
+                    this.stop_playing();
+                }
+            }
+        };
+        return radio;
+    }
 });
